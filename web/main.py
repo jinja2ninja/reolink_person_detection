c8619015db6089deb8a89c57cc7813b01646be20
Mischa Friegang <mischatravis10@gmail.com>
2024-07-13 11:45:37 -0700

    replace web UI with nicegui

--- web/main.py
+++ web/main.py
@@ -1,51 +1,39 @@
-from fastapi import FastAPI, UploadFile, File, Form, APIRouter, Request
-from fastapi.middleware.cors import CORSMiddleware
+#!/usr/bin/env python3
+from nicegui import ui
 import psycopg2
-from psycopg2 import sql
-from fastapi.responses import HTMLResponse, FileResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-#from sqlalchemy import Column, Integer, MetaData, String, Table, select
-import os
-import logging
-import psycopg2
-import asyncio
 from psycopg2 import sql
 from psycopg2.extensions import AsIs
-import re
+from itertools import cycle
 import hvac
-
+import os
+import logging
+import re
 ############################
 # Hashicorp Vault Secret Retrevial
 ############################
 client = hvac.Client(
         url='https://vault.mischaf.us'
         )
-
 client.auth.approle.login(
     role_id=os.environ['ROLE_ID'],
     secret_id=os.environ['SECRET_ID'],
 )
-
-secrets = client.kv.read_secret(path='detector')
+secrets = client.secrets.kv.v2.read_secret(path='detector')
 logging.info(secrets)
-secrets = client.kv.read_secret(path='detector')
+secrets = client.secrets.kv.v2.read_secret(path='detector')
 
 ############################
-# Variable Declaration
+# Logging Config
 ############################
 logging.basicConfig()
-logging.getLogger().setLevel("INFO")
-templates = Jinja2Templates(directory="/web/app/templates")
-app = FastAPI()
-app.mount("/photos", StaticFiles(directory="/web/app/photos"), name="photos")
-app.mount("/templates", StaticFiles(directory="/web/app/templates"), name="templates")
-api_router = APIRouter()
+logging.getLogger().setLevel("DEBUG")
 
-############################
-# Retrieve DB Rows for "latest" page
-############################
-async def read_latest_rows(camera):
+
+##############################
+# Images List
+##############################
+## create images list
+def read_latest_rows(camera):
   try:
       connection = psycopg2.connect(user=secrets['data']['data']["pg_user"],
                                     password=secrets['data']['data']["pg_pass"],
@@ -57,69 +45,26 @@ async def read_latest_rows(camera):
       cursor.execute(
           """
         SELECT * from %s
-        ORDER BY TIMESTAMP DESC LIMIT 5;
+        ORDER BY TIMESTAMP DESC;
           """,
           [AsIs(table)]
       )
       latest_rows = cursor.fetchall()
-      logging.debug(latest_rows)
+      #logging.debug(latest_rows)
       return latest_rows
   except (Exception, psycopg2.Error) as error:
       logging.error("Error while fetching data from PostgreSQL", error)
-
-############################
-# Retrieve DB Rows for "detection" page
-############################
-async def read_rows(filename, camera):
-  try:
-      connection = psycopg2.connect(user=secrets['data']['data']["pg_user"],
-                                    password=secrets['data']['data']["pg_pass"],
-                                    host=secrets['data']['data']["pg_host"],
-                                    port="5432",
-                                    database="detector")
-      cursor = connection.cursor()
-      table = camera.replace("'", "")
-      cursor.execute(
-          """
-        WITH init AS (
-            SELECT timestamp
-            FROM %s
-            WHERE filename = %s
-         )
-         
-         (
-            (SELECT %s.*
-             FROM %s
-                CROSS JOIN init
-             WHERE %s.timestamp > init.timestamp
-             ORDER BY %s.timestamp DESC 
-             LIMIT 2)
-           UNION ALL
-         (
-            SELECT %s.*
-            FROM %s
-            WHERE filename = %s
-         )
-          UNION ALL 
-            (SELECT %s.*
-             FROM %s
-                CROSS JOIN init 
-             WHERE %s.timestamp < init.timestamp
-             ORDER BY %s.timestamp DESC
-             LIMIT 2)
-         );
-          """,
-          [AsIs(table), filename, AsIs(table), AsIs(table), AsIs(table), AsIs(table), AsIs(table), AsIs(table), filename, AsIs(table), AsIs(table), AsIs(table), AsIs(table)]
-      )
-      rows = cursor.fetchall()
-      return rows
-  except (Exception, psycopg2.Error) as error:
-      logging.error("Error while fetching data from PostgreSQL", error)
-
-############################
-# Retrieve list of cameras from DB
-############################
-async def get_cameras():
+def create_images_list(camera):
+  latest_rows = read_latest_rows(camera)
+  #image_paths = list(latest_rows[0])
+  image_paths = []
+  for row in latest_rows:
+    image_paths.append(row[0])
+  return(image_paths)
+#####################################
+# Camera List
+#####################################
+def get_cameras():
   try:
       connection = psycopg2.connect(user=secrets['data']['data']["pg_user"],
                                     password=secrets['data']['data']["pg_pass"],
@@ -139,29 +84,61 @@ async def get_cameras():
       camera_names = []
       for name in cameras:
           camera_name = re.sub(r'\'\,\)', '', re.sub(r'\(\'', '', str(name)))
-          logging.debug(camera_name)
+          #logging.debug(camera_name)
           camera_names.append(camera_name)
       return camera_names
   except (Exception, psycopg2.Error) as error:
       logging.error("Error while fetching data from PostgreSQL", error)
 
-############################
-# "Latest" Page
-############################
-@app.get("/{camera}/detection/latest", response_class=HTMLResponse)
-async def latest(request: Request, camera: str):
-  latest_rows = await read_latest_rows(camera)
-  filename = latest_rows[0][0]
-  cameras_list = await get_cameras()
-  return templates.TemplateResponse("detection_template_latest.html", {"request": request, "cameras_list": cameras_list, "filename": filename, "latest_rows": latest_rows, "camera": camera})
+#######################################
+# Create Image Lists
+#######################################
+cameras = get_cameras()
+images = {}
+for camera in cameras:
+  image_list = create_images_list(camera)
+  images[f'{camera}'] = image_list
 
-############################
-# "Detection" Page
-############################
-@app.get("/{camera}/detection/{filename}", response_class=HTMLResponse)
-async def detection(request: Request, filename: str, camera: str):
-  filename_full = f"./photos/{camera}/{filename}.jpeg"
-  rows = await read_rows(filename_full, camera)
-  cameras_list = await get_cameras()
-  return templates.TemplateResponse("detection_template.html", {"request": request, "cameras_list": cameras_list, "filename": filename, "rows": rows, "camera": camera})
+print(images)
 
+######################################
+# Nice UI
+######################################
+@ui.page('/', response_timeout = 99)
+
+async def page():
+    async def check(image_iterator, camera):
+        try:
+            result = await ui.run_javascript('window.pageYOffset >= document.body.offsetHeight - 2 * window.innerHeight')
+            logging.debug(f'JavaScript result for {camera}: {result}')
+            if result:
+                next_image = next(image_iterator)
+                logging.debug(f'Next image for {camera}: {next_image}')
+                ui.image(next_image)
+        except TimeoutError:
+            logging.error('TimeoutError: The client might have disconnected')
+        except Exception as e:
+            logging.error(f'Exception occurred: {e}')
+
+    await ui.context.client.connected()
+    dark = ui.dark_mode()
+    ui.page_title('Person Detector')
+    with ui.header(elevated=True).style('background-color: #3874c8').classes('items-center justify-between'):
+        ui.label('Person Detector')
+        with ui.tabs() as tabs:
+          for camera in cameras:
+              ui.tab(re.sub(r'_', ' ', camera))
+        with ui.row():
+          ui.button('Dark Mode', on_click=dark.enable)
+          ui.button('Light Mode', on_click=dark.disable)
+    with ui.footer(value=False) as footer:
+        ui.label('Footer')
+    with ui.page_sticky(position='bottom-right', x_offset=20, y_offset=20):
+        ui.button(on_click=footer.toggle, icon='contact_support').props('fab')
+    with ui.tab_panels(tabs, value=cameras[0]).classes('w-full'):
+        for camera in cameras:
+            with ui.tab_panel(re.sub(r'_', ' ', camera)):
+                image_iterator = cycle(images[camera])
+                ui.timer(1.1, lambda it=image_iterator, cam=camera: check(it, cam))
+
+ui.run()
